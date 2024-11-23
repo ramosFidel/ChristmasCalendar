bea23cc74c92a82bee75d1b8641ded9b051d2cbc
Fidel Eduardo Ramos Bajaña <fidel.ramosb@ug.edu.ec>
2024-11-22 23:11:33 -0500

    update

--- main.js
+++ main.js
@@ -93,6 +93,34 @@ document.querySelector('.day-4').addEventListener('click', function() {
 });
 
 document.querySelector('.day-5').addEventListener('click', function() {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    origin: { y: 0.20, x: 0.55 },
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+  
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
 });
 document.querySelector('.day-6').addEventListener('click', function() {
   const duration = 5 * 1000,
